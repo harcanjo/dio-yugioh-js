262af3a8f55b36ce22d5d2990d3d9011ab632d81
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-20 16:49:06 -0300

    feat: remove cards fields

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -13,14 +13,15 @@ const state = {
     player: document.getElementById("player-field-card"),
     computer: document.getElementById("computer-field-card"),
   },
+  playerSides: {
+    player1: "player-cards",
+    player1Box: document.querySelector("#player-cards"),
+    computer: "computer-cards",
+    computerBox: document.querySelector("#computer-cards"),
+  },
   actions: {
     button: document.getElementById("next-duel"),
   }
-};
-
-const playerSides = {
-  player1: "player-cards",
-  computer: "computer-cards",
 };
 
 const pathImages = "./src/assets/icons/";
@@ -63,7 +64,7 @@ async function createCardImage(idCard, fieldSide) {
   cardImage.setAttribute("data-id", idCard);
   cardImage.classList.add("card");
 
-  if (fieldSide === playerSides.player1) {
+  if (fieldSide === state.playerSides.player1) {
     cardImage.addEventListener("mouseover", () => {
       drawSelectedCard(idCard);
     });
@@ -82,6 +83,33 @@ async function drawSelectedCard(index) {
   state.cardSprites.type.innerText = "Attribute : " + cardData[index].type;
 }
 
+async function setCardsField(cardId) {
+  await removeAllCardsImages();
+
+  let computerCardId = await getRandomCardId();
+
+  state.fieldCards.player.style.display = "block";
+  state.fieldCards.computer.style.display = "block";
+
+  state.fieldCards.player.src = cardData[cardId].img;
+  state.fieldCards.computer.src = cardData[computerCardId].img;
+
+  let duelResult = await checkDuelResults(cardId, computerCardId);
+
+  await updateScore();
+  await drawButton(duelResult);
+}
+
+async function removeAllCardsImages() {
+  let { computerBox, player1Box } = state.playerSides;
+
+  let imgElements = computerBox.querySelectorAll("img");
+  imgElements.forEach((img) => img.remove());
+
+  imgElements = player1Box.querySelectorAll("img");
+  imgElements.forEach((img) => img.remove());
+}
+
 async function drawCards(cardNumbers, fieldSide) {
   for (let i = 0; i < cardNumbers; i++) {
     const randomIdCard = await getRandomCardId();
@@ -92,8 +120,8 @@ async function drawCards(cardNumbers, fieldSide) {
 }
 
 function init() {
-  drawCards(5, playerSides.player1);
-  drawCards(5, playerSides.computer);
+  drawCards(5, state.playerSides.player1);
+  drawCards(5, state.playerSides.computer);
 };
 
 init();
